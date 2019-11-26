f8838a9b2b0561036ff5518db4c29cb5ca5de57d
Stoorx <antiproton2013@yandex.ru>
2019-11-26 18:52:54 +0300

    Add functional tests

--- index.js
+++ index.js
@@ -95,3 +95,64 @@ const repackData = (obj) => ({
     icon: obj.weather[0].icon
 });
 
+const example = () => ({
+    "coord": {
+        "lon": 37.62,
+        "lat": 55.75
+    },
+    "weather": [
+        {
+            "id": 800,
+            "main": "Clear",
+            "description": "clear sky",
+            "icon": "01n"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 277.37,
+        "pressure": 1017,
+        "humidity": 86,
+        "temp_min": 276.48,
+        "temp_max": 278.15
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 2,
+        "deg": 240
+    },
+    "clouds": {
+        "all": 0
+    },
+    "dt": 1569349865,
+    "sys": {
+        "type": 1,
+        "id": 9029,
+        "message": 0.0065,
+        "country": "RU",
+        "sunrise": 1569295059,
+        "sunset": 1569338749
+    },
+    "timezone": 10800,
+    "id": 524901,
+    "name": "Moscow",
+    "cod": 200
+});
+
+const correctRepacked = () => ({
+    name: "Moscow",
+    country: "RU",
+    temp: 277.37,
+    wind: {
+        speed: 2,
+        heading: 240
+    },
+    clouds: "clear sky",
+    pressure: 1017,
+    humidity: 86,
+    coords: {
+        lat: 55.75,
+        lon: 37.62
+    },
+    icon: "01n"
+});
